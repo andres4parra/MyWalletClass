0f4f5b156afa011c68289240b8d7ec8fb620fddc
andres4parra <116411931+andres4parra@users.noreply.github.com>
2023-10-13 09:20:13 -0500

    tarea

--- app/src/main/java/com/fabiansuarez/mywallet/LoginActivity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/LoginActivity.java
@@ -9,18 +9,33 @@ import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btn1,btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn1=(Button) findViewById(R.id.btn_log);
+        btn2=(Button) findViewById(R.id.btn_reg);
 
 
-        Button boton  = findViewById(R.id.btn_register);
-        boton.setOnClickListener(new View.OnClickListener()){
-            public void onClick(View v){
-                Intent intent= new Intent(this , RegisterActivity);
-                startActivity(intent);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent= new Intent(LoginActivity.this, CategoryActivity.class);
+                startActivity(myIntent);
+
             }
-        }
+
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent= new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(myIntent);
+
+            }
+
+        });
+
     }
 }
--- app/src/main/java/com/fabiansuarez/mywallet/RegisterActivity.java
+++ app/src/main/java/com/fabiansuarez/mywallet/RegisterActivity.java
@@ -2,13 +2,29 @@ package com.fabiansuarez.mywallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        btn=(Button)findViewById(R.id.btn_register);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent= new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(myIntent);
+
+            }
+
+        });
+
+
     }
 }
